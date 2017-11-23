9e9f7641ce6941fdfb7f62f2e176b917a43ff473
Mario vizcaino <mario.vizcainos@gmail.com>
2017-11-23 17:46:48 -0500

    setup create expense

--- app/assets/javascripts/expenses.js
+++ app/assets/javascripts/expenses.js
@@ -1,5 +1,5 @@
-$(document).on('turbolinks:load', function () {
-  console.log('loading vals')
+$(document).on('turbolinks:load', () => {
+  console.log('loading vals') 
   $('#expense-form').validate({
     rules: {
       'date': {required: true},
@@ -7,9 +7,35 @@ $(document).on('turbolinks:load', function () {
       'concept': {required: true},
       'transaction_type_id': {required: true},
       'category_id': {required: true}
+    },
+    submitHandler: () => {
+      let formData = $('#expense-form').serializeArray() // store json object
+      let obj = {}
+      formData.forEach(element => {
+        obj[element['name']] = element['value']
+      })
+      console.log(obj)
+      $.ajax({
+        type: 'POST',
+        url: '/api/v1/expenses',
+        dataType: 'json',
+        contentType: 'application/json',
+        data: JSON.stringify(obj),
+        success: function () {
+          swal({
+            title: 'Gasto Guardado',
+            type: 'success',
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'OK'
+          }).then(function (result) {
+            document.location.reload()            
+          })
+        }
+      })
     }
   })
-  $('.delete-btn').click(function () {
+  $('.delete-btn').click(() => {
     let id = $(this).val()
     swal.queue([{
       title: 'Eliminar',
@@ -30,6 +56,6 @@ $(document).on('turbolinks:load', function () {
       }
     }])
   })
-  $('.edit-btn').click(function () {
+  $('.edit-btn').click(() => {
   })
 })
