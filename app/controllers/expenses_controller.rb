3501da8fe0792d44bb6f7ddba6099240c391bcda
Mario vizcaino <mario.vizcainos@gmail.com>
2017-11-28 12:39:55 -0500

    setup the epense params to allow date

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -5,7 +5,7 @@ class ExpensesController < ApplicationController
   def index
     @title = "Mis Gastos"
     @tab = :expenses
-    @expenses = Expense.with_transaction_type_id(params[:transaction_type_id]).with_category_id(params[:category_id])
+    @expenses = Expense.with_transaction_type_id(params[:transaction_type_id]).with_category_id(params[:category_id]).with_month(params[:month]).with_year(params[:year])
     @categories= Category.all
     @types = TransactionType.all
     @months = last_12_months
