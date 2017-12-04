b84206781ecaae1776ec5278b9febef41f63c96f
Mario vizcaino <mario.vizcainos@gmail.com>
2017-12-04 10:18:45 -0500

    by category char

--- app/assets/javascripts/dashboard.js
+++ app/assets/javascripts/dashboard.js
@@ -2,4 +2,57 @@ $(document).on('turbolinks:load', function () {
   $('.header-link').click(function () {
     localStorage.removeItem('date_selected')
   })
+  let charts = new Charts()
+  charts.categoryData()
+})
+class Charts {
+  chartByCategory (data) {
+    let options = {
+      responsive: true,
+      animation: {
+        animateScale: true,
+        animateRotate: true
+      },
+      pieceLabel: {
+        fontColor: '#FFF'
+      },
+      tooltips: {
+        enabled: true,
+        mode: 'single',
+        callbacks: {
+          label: function (tooltipItems, data) {
+            return formatter.format(data.datasets[0].data[tooltipItems.index])
+          }
+        }
+      }
+    }
+    let d = {
+      datasets: [{
+        data: data.values,
+        backgroundColor: data.backgroundColors
+      }],
+      // These labels appear in the legend and in the tooltips when hovering different arcs
+      labels: data.labels
+    }
+    let ctx = document.getElementById('byCategoryChar')// .getContext('2d')
+    let myDoughnutChart = new Chart (ctx, {
+      type: 'doughnut',
+      data: d,
+      options: options
+    })
+  }
+  categoryData () {
+    fetch('/chart/categories.json')
+    .then(response => response.json())
+    .then(json => this.chartByCategory(json.categories))
+    return 1
+  }
+}
+
+let formatter = new Intl.NumberFormat('es-CO', {
+  style: 'currency',
+  currency: 'COP',
+  minimumFractionDigits: 2
+  // the default value for minimumFractionDigits depends on the currency
+  // and is usually already 2
 })
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -9,13 +9,20 @@ class DashboardController < ApplicationController
   def by_category
     categories = Category.all
     @categories = []
-    colors = ["#3F51B5", "#673AB7", "#2196F3", "8BC34A", "4CAF50", "CDDC39", "FF9800"]
+    colors = ["#3F51B5", "#673AB7", "#2196F3", "#8BC34A", "#4CAF50", "#CDDC39", "#FF9800"]
+    labels = []
+    data = []
+    bg = []
     categories.each_with_index do |category, index|
-      @categories.push({
-        label: category.name,
-        value: category.expenses.from_this_month.count,
-        color: colors[index]
-      })
+      labels.push(category.name)
+      data.push(category.expenses.from_this_month.sum(:amount))
+      bg.push(colors[index])
     end
+    @categories = ({
+      values: data,
+      backgroundColors: bg,
+      labels: labels
+    })
+
   end
 end
