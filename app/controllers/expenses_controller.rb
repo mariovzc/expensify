b84771cdae4fa07dd171354dca2fa58bf3357de3
Mario vizcaino <mario.vizcainos@gmail.com>
2017-12-05 17:39:37 -0500

    order by date ASC

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -7,7 +7,7 @@ class ExpensesController < ApplicationController
     @tab = :expenses
     year = params[:year].present? ? params[:year] : Time.current.year 
     month = params[:month].present? ? params[:month] : Time.current.month
-    @expenses = Expense.with_transaction_type_id(params[:transaction_type_id]).with_category_id(params[:category_id]).with_month(month).with_year(year).paginate(:page => params[:page], :per_page => 10)
+    @expenses = Expense.with_transaction_type_id(params[:transaction_type_id]).with_category_id(params[:category_id]).with_month(month).with_year(year).paginate(:page => params[:page], :per_page => 10).order('date ASC')
     @categories= Category.all
     @types = TransactionType.all
     @months = last_12_months
