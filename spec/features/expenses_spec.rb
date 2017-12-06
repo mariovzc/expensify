5dc9db19574614ef3819318769a4496b3b0527c6
Mario vizcaino <mario.vizcainos@gmail.com>
2017-12-06 16:32:25 -0500

    filter by transaction and category tests

--- spec/features/expenses_spec.rb
+++ spec/features/expenses_spec.rb
@@ -1,6 +1,17 @@
 require "rails_helper"
 
 RSpec.feature "Expenses", :type => :feature do
+=begin
+  scenario "User creates a new Expense" do
+    visit "/expenses"
+    
+    click_button "Nuevo Gasto"
+    
+    click_button "Guardar"
+
+    expect(page).to have_text("Mis Gastos")
+  end
+=end
   scenario "User filter by date" do
     visit "/expenses"
     within '#month_selector' do
@@ -12,5 +23,17 @@ RSpec.feature "Expenses", :type => :feature do
       expected = "/expenses?utf8=✓&month=#{Time.now.month}&year=#{Time.now.year}&transaction_type_id=&category_id="
       expect(page).to have_current_path(expected, url: false)
     end
+  end
+  scenario "User filter by transaction type" do
+    visit "/expenses"      
+    choose('transaction_type_id1', allow_label_click: true) # find by id and select by clicking the label if input is non-visible
+      expected = "/expenses?utf8=✓&month=#{Time.now.month}&year=#{Time.now.year}&transaction_type_id=1&category_id="    
+      expect(page).to have_current_path(expected, url: false)      
+  end
+  scenario "User filter by category_type type" do
+    visit "/expenses"      
+    choose('category_id_1', allow_label_click: true) # find by id and select by clicking the label if input is non-visible
+      expected = "/expenses?utf8=✓&month=#{Time.now.month}&year=#{Time.now.year}&transaction_type_id=&category_id=1"    
+      expect(page).to have_current_path(expected, url: false)      
   end
 end
