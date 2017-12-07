84a3629bcc0d4829d3b58734d8ab95ce0025d77c
Mario vizcaino <mario.vizcainos@gmail.com>
2017-12-06 21:40:00 -0500

    some configs for capybara

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -69,6 +69,7 @@ RSpec.configure do |config|
   end
   
   Capybara.javascript_driver = :selenium_chrome
+  Capybara.default_wait_time = 10
   
   Capybara.configure do |config|
     config.default_max_wait_time = 10 # seconds
