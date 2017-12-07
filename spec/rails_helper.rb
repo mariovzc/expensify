0dfdcc8a064699c5d0a13f01aebbb9c8d421894d
Mario vizcaino <mario.vizcainos@gmail.com>
2017-12-06 21:21:39 -0500

    changed the drivers of selenium

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -68,7 +68,7 @@ RSpec.configure do |config|
     Capybara::Selenium::Driver.new(app, browser: :chrome)
   end
   
-  Capybara.javascript_driver = :chrome
+  Capybara.javascript_driver = :selenium_chrome
   
   Capybara.configure do |config|
     config.default_max_wait_time = 10 # seconds
